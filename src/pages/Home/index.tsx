a5d9e41b7919ef71d31aa1eab6194aff1d24490a
Hernany Motoso <contatohernany@gmail.com>
2022-03-28 21:49:07 -0300

    feat: initial setup
    Created the route system and default home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Home:React.FC = () => {
+    console.log('Home-page');
+
+    return (
+        <Container>
+            <h1>Home</h1>
+            <h1>Page</h1>
+        </Container>
+    )
+}
+
+
+export default Home;
