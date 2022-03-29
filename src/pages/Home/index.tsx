ef497b9e152d1b7a905fe9952dfb9dcf29a96c94
Hernany Motoso <contatohernany@gmail.com>
2022-03-29 12:37:10 -0300

    feat: initial setup
    Configured code patterns eslint, prettier and editor config

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 
 import { Container } from './styles';
 
-const Home:React.FC = () => {
-    console.log('Home-page');
+const Home: React.FC = () => {
+  console.log('Home-page');
 
-    return (
-        <Container>
-            <h1>Home</h1>
-            <h1>Page</h1>
-        </Container>
-    )
-}
-
+  return (
+    <Container>
+      <h1>Home</h1>
+      <h1>Page</h1>
+    </Container>
+  );
+};
 
 export default Home;
