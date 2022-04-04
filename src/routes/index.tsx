36749935bf09ca622f0a0be270fcc36f20971c02
Hernany Motoso <contatohernany@gmail.com>
2022-04-04 18:00:52 -0300

    feat: config .end and config Procfile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 
 import { Routes as Switch, Route } from 'react-router-dom';
 
-
 import Home from '../pages/Home';
 
-
 const Routes: React.FC = () => (
-    <Switch>
-        <Route path="/" element={<Home/>} />
-    </Switch>
-)
-
-
+  <Switch>
+    <Route path="/" element={<Home />} />
+  </Switch>
+);
 
 export default Routes;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_APP_API_URL;
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: API_URL as string,
 });
 
 export default api;
